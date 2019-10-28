7a35c109ecc02764543c4b91d422cbdfc3a1df24
Dmitry Vibe <riim@yandex.ru>
2019-10-28 23:11:42 +0300

    add opal-form to opal-components

--- packages/opal-components/dist/index.js
+++ packages/opal-components/dist/index.js
@@ -1,13 +1,13 @@
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory(require("@riim/opal-group"), require("@riim/opal-text-input-validator"), require("@riim/opal-notification"), require("@riim/opal-popover"), require("@riim/opal-editable-text"), require("@riim/opal-radio-group"), require("@riim/opal-tree-select"), require("@riim/opal-switch-menu"), require("@riim/opal-checkbox"), require("@riim/opal-input-validator"), require("@riim/opal-dropdown"), require("@riim/opal-switch"), require("@riim/opal-loader"), require("@riim/opal-autosuggest"), require("@riim/opal-input-mask"), require("@riim/opal-focus-highlight-controller"), require("@riim/opal-slider"), require("@riim/opal-file-upload"), require("@riim/opal-date-input"), require("@riim/opal-router"), require("@riim/opal-select-validator"), require("@riim/opal-button"), require("@riim/opal-icon"), require("@riim/opal-filtered-list"), require("@riim/opal-multirow"), require("@riim/opal-tag-select"), require("@riim/opal-sign-button"), require("@riim/opal-text-input"), require("@riim/opal-calendar"), require("@riim/opal-tree-tag-select"), require("@riim/opal-loaded-list"), require("@riim/opal-select"), require("@riim/opal-multiselect"), require("@riim/opal-utils"), require("@riim/opal-tabs"), require("@riim/opal-tree-list"), require("@riim/opal-modal"));
+		module.exports = factory(require("@riim/opal-group"), require("@riim/opal-text-input-validator"), require("@riim/opal-notification"), require("@riim/opal-popover"), require("@riim/opal-editable-text"), require("@riim/opal-radio-group"), require("@riim/opal-tree-select"), require("@riim/opal-switch-menu"), require("@riim/opal-checkbox"), require("@riim/opal-input-validator"), require("@riim/opal-dropdown"), require("@riim/opal-switch"), require("@riim/opal-loader"), require("@riim/opal-autosuggest"), require("@riim/opal-input-mask"), require("@riim/opal-focus-highlight-controller"), require("@riim/opal-slider"), require("@riim/opal-file-upload"), require("@riim/opal-date-input"), require("@riim/opal-router"), require("@riim/opal-select-validator"), require("@riim/opal-button"), require("@riim/opal-icon"), require("@riim/opal-filtered-list"), require("@riim/opal-multirow"), require("@riim/opal-tag-select"), require("@riim/opal-sign-button"), require("@riim/opal-text-input"), require("@riim/opal-calendar"), require("@riim/opal-tree-tag-select"), require("@riim/opal-loaded-list"), require("@riim/opal-select"), require("@riim/opal-multiselect"), require("@riim/opal-utils"), require("@riim/opal-tabs"), require("@riim/opal-tree-list"), require("rionite"), require("@riim/opal-modal"));
 	else if(typeof define === 'function' && define.amd)
-		define(["@riim/opal-group", "@riim/opal-text-input-validator", "@riim/opal-notification", "@riim/opal-popover", "@riim/opal-editable-text", "@riim/opal-radio-group", "@riim/opal-tree-select", "@riim/opal-switch-menu", "@riim/opal-checkbox", "@riim/opal-input-validator", "@riim/opal-dropdown", "@riim/opal-switch", "@riim/opal-loader", "@riim/opal-autosuggest", "@riim/opal-input-mask", "@riim/opal-focus-highlight-controller", "@riim/opal-slider", "@riim/opal-file-upload", "@riim/opal-date-input", "@riim/opal-router", "@riim/opal-select-validator", "@riim/opal-button", "@riim/opal-icon", "@riim/opal-filtered-list", "@riim/opal-multirow", "@riim/opal-tag-select", "@riim/opal-sign-button", "@riim/opal-text-input", "@riim/opal-calendar", "@riim/opal-tree-tag-select", "@riim/opal-loaded-list", "@riim/opal-select", "@riim/opal-multiselect", "@riim/opal-utils", "@riim/opal-tabs", "@riim/opal-tree-list", "@riim/opal-modal"], factory);
+		define(["@riim/opal-group", "@riim/opal-text-input-validator", "@riim/opal-notification", "@riim/opal-popover", "@riim/opal-editable-text", "@riim/opal-radio-group", "@riim/opal-tree-select", "@riim/opal-switch-menu", "@riim/opal-checkbox", "@riim/opal-input-validator", "@riim/opal-dropdown", "@riim/opal-switch", "@riim/opal-loader", "@riim/opal-autosuggest", "@riim/opal-input-mask", "@riim/opal-focus-highlight-controller", "@riim/opal-slider", "@riim/opal-file-upload", "@riim/opal-date-input", "@riim/opal-router", "@riim/opal-select-validator", "@riim/opal-button", "@riim/opal-icon", "@riim/opal-filtered-list", "@riim/opal-multirow", "@riim/opal-tag-select", "@riim/opal-sign-button", "@riim/opal-text-input", "@riim/opal-calendar", "@riim/opal-tree-tag-select", "@riim/opal-loaded-list", "@riim/opal-select", "@riim/opal-multiselect", "@riim/opal-utils", "@riim/opal-tabs", "@riim/opal-tree-list", "rionite", "@riim/opal-modal"], factory);
 	else if(typeof exports === 'object')
-		exports["@riim/opal-components"] = factory(require("@riim/opal-group"), require("@riim/opal-text-input-validator"), require("@riim/opal-notification"), require("@riim/opal-popover"), require("@riim/opal-editable-text"), require("@riim/opal-radio-group"), require("@riim/opal-tree-select"), require("@riim/opal-switch-menu"), require("@riim/opal-checkbox"), require("@riim/opal-input-validator"), require("@riim/opal-dropdown"), require("@riim/opal-switch"), require("@riim/opal-loader"), require("@riim/opal-autosuggest"), require("@riim/opal-input-mask"), require("@riim/opal-focus-highlight-controller"), require("@riim/opal-slider"), require("@riim/opal-file-upload"), require("@riim/opal-date-input"), require("@riim/opal-router"), require("@riim/opal-select-validator"), require("@riim/opal-button"), require("@riim/opal-icon"), require("@riim/opal-filtered-list"), require("@riim/opal-multirow"), require("@riim/opal-tag-select"), require("@riim/opal-sign-button"), require("@riim/opal-text-input"), require("@riim/opal-calendar"), require("@riim/opal-tree-tag-select"), require("@riim/opal-loaded-list"), require("@riim/opal-select"), require("@riim/opal-multiselect"), require("@riim/opal-utils"), require("@riim/opal-tabs"), require("@riim/opal-tree-list"), require("@riim/opal-modal"));
+		exports["@riim/opal-components"] = factory(require("@riim/opal-group"), require("@riim/opal-text-input-validator"), require("@riim/opal-notification"), require("@riim/opal-popover"), require("@riim/opal-editable-text"), require("@riim/opal-radio-group"), require("@riim/opal-tree-select"), require("@riim/opal-switch-menu"), require("@riim/opal-checkbox"), require("@riim/opal-input-validator"), require("@riim/opal-dropdown"), require("@riim/opal-switch"), require("@riim/opal-loader"), require("@riim/opal-autosuggest"), require("@riim/opal-input-mask"), require("@riim/opal-focus-highlight-controller"), require("@riim/opal-slider"), require("@riim/opal-file-upload"), require("@riim/opal-date-input"), require("@riim/opal-router"), require("@riim/opal-select-validator"), require("@riim/opal-button"), require("@riim/opal-icon"), require("@riim/opal-filtered-list"), require("@riim/opal-multirow"), require("@riim/opal-tag-select"), require("@riim/opal-sign-button"), require("@riim/opal-text-input"), require("@riim/opal-calendar"), require("@riim/opal-tree-tag-select"), require("@riim/opal-loaded-list"), require("@riim/opal-select"), require("@riim/opal-multiselect"), require("@riim/opal-utils"), require("@riim/opal-tabs"), require("@riim/opal-tree-list"), require("rionite"), require("@riim/opal-modal"));
 	else
-		root["@riim/opal-components"] = factory(root["@riim/opal-group"], root["@riim/opal-text-input-validator"], root["@riim/opal-notification"], root["@riim/opal-popover"], root["@riim/opal-editable-text"], root["@riim/opal-radio-group"], root["@riim/opal-tree-select"], root["@riim/opal-switch-menu"], root["@riim/opal-checkbox"], root["@riim/opal-input-validator"], root["@riim/opal-dropdown"], root["@riim/opal-switch"], root["@riim/opal-loader"], root["@riim/opal-autosuggest"], root["@riim/opal-input-mask"], root["@riim/opal-focus-highlight-controller"], root["@riim/opal-slider"], root["@riim/opal-file-upload"], root["@riim/opal-date-input"], root["@riim/opal-router"], root["@riim/opal-select-validator"], root["@riim/opal-button"], root["@riim/opal-icon"], root["@riim/opal-filtered-list"], root["@riim/opal-multirow"], root["@riim/opal-tag-select"], root["@riim/opal-sign-button"], root["@riim/opal-text-input"], root["@riim/opal-calendar"], root["@riim/opal-tree-tag-select"], root["@riim/opal-loaded-list"], root["@riim/opal-select"], root["@riim/opal-multiselect"], root["@riim/opal-utils"], root["@riim/opal-tabs"], root["@riim/opal-tree-list"], root["@riim/opal-modal"]);
-})(window, function(__WEBPACK_EXTERNAL_MODULE__DRg__, __WEBPACK_EXTERNAL_MODULE__tCi__, __WEBPACK_EXTERNAL_MODULE__01ri__, __WEBPACK_EXTERNAL_MODULE__2L3N__, __WEBPACK_EXTERNAL_MODULE__44oX__, __WEBPACK_EXTERNAL_MODULE__5WFK__, __WEBPACK_EXTERNAL_MODULE__6dQR__, __WEBPACK_EXTERNAL_MODULE__6upG__, __WEBPACK_EXTERNAL_MODULE__7ATV__, __WEBPACK_EXTERNAL_MODULE__9L4b__, __WEBPACK_EXTERNAL_MODULE_A7KY__, __WEBPACK_EXTERNAL_MODULE_B_Ww__, __WEBPACK_EXTERNAL_MODULE_BLMt__, __WEBPACK_EXTERNAL_MODULE_FoNw__, __WEBPACK_EXTERNAL_MODULE_JK3___, __WEBPACK_EXTERNAL_MODULE_Lgwm__, __WEBPACK_EXTERNAL_MODULE_LoPe__, __WEBPACK_EXTERNAL_MODULE_MZUI__, __WEBPACK_EXTERNAL_MODULE_OBm1__, __WEBPACK_EXTERNAL_MODULE_Oufu__, __WEBPACK_EXTERNAL_MODULE_RZve__, __WEBPACK_EXTERNAL_MODULE_X6DG__, __WEBPACK_EXTERNAL_MODULE_Y0hN__, __WEBPACK_EXTERNAL_MODULE_Y1_I__, __WEBPACK_EXTERNAL_MODULE_aYPB__, __WEBPACK_EXTERNAL_MODULE_cu1Y__, __WEBPACK_EXTERNAL_MODULE_ej3F__, __WEBPACK_EXTERNAL_MODULE_hl6F__, __WEBPACK_EXTERNAL_MODULE_iMF9__, __WEBPACK_EXTERNAL_MODULE_iURx__, __WEBPACK_EXTERNAL_MODULE_ioIi__, __WEBPACK_EXTERNAL_MODULE_jL6t__, __WEBPACK_EXTERNAL_MODULE_mdH0__, __WEBPACK_EXTERNAL_MODULE_qQaz__, __WEBPACK_EXTERNAL_MODULE_qhKH__, __WEBPACK_EXTERNAL_MODULE_qhWZ__, __WEBPACK_EXTERNAL_MODULE_y39J__) {
+		root["@riim/opal-components"] = factory(root["@riim/opal-group"], root["@riim/opal-text-input-validator"], root["@riim/opal-notification"], root["@riim/opal-popover"], root["@riim/opal-editable-text"], root["@riim/opal-radio-group"], root["@riim/opal-tree-select"], root["@riim/opal-switch-menu"], root["@riim/opal-checkbox"], root["@riim/opal-input-validator"], root["@riim/opal-dropdown"], root["@riim/opal-switch"], root["@riim/opal-loader"], root["@riim/opal-autosuggest"], root["@riim/opal-input-mask"], root["@riim/opal-focus-highlight-controller"], root["@riim/opal-slider"], root["@riim/opal-file-upload"], root["@riim/opal-date-input"], root["@riim/opal-router"], root["@riim/opal-select-validator"], root["@riim/opal-button"], root["@riim/opal-icon"], root["@riim/opal-filtered-list"], root["@riim/opal-multirow"], root["@riim/opal-tag-select"], root["@riim/opal-sign-button"], root["@riim/opal-text-input"], root["@riim/opal-calendar"], root["@riim/opal-tree-tag-select"], root["@riim/opal-loaded-list"], root["@riim/opal-select"], root["@riim/opal-multiselect"], root["@riim/opal-utils"], root["@riim/opal-tabs"], root["@riim/opal-tree-list"], root["rionite"], root["@riim/opal-modal"]);
+})(window, function(__WEBPACK_EXTERNAL_MODULE__DRg__, __WEBPACK_EXTERNAL_MODULE__tCi__, __WEBPACK_EXTERNAL_MODULE__01ri__, __WEBPACK_EXTERNAL_MODULE__2L3N__, __WEBPACK_EXTERNAL_MODULE__44oX__, __WEBPACK_EXTERNAL_MODULE__5WFK__, __WEBPACK_EXTERNAL_MODULE__6dQR__, __WEBPACK_EXTERNAL_MODULE__6upG__, __WEBPACK_EXTERNAL_MODULE__7ATV__, __WEBPACK_EXTERNAL_MODULE__9L4b__, __WEBPACK_EXTERNAL_MODULE_A7KY__, __WEBPACK_EXTERNAL_MODULE_B_Ww__, __WEBPACK_EXTERNAL_MODULE_BLMt__, __WEBPACK_EXTERNAL_MODULE_FoNw__, __WEBPACK_EXTERNAL_MODULE_JK3___, __WEBPACK_EXTERNAL_MODULE_Lgwm__, __WEBPACK_EXTERNAL_MODULE_LoPe__, __WEBPACK_EXTERNAL_MODULE_MZUI__, __WEBPACK_EXTERNAL_MODULE_OBm1__, __WEBPACK_EXTERNAL_MODULE_Oufu__, __WEBPACK_EXTERNAL_MODULE_RZve__, __WEBPACK_EXTERNAL_MODULE_X6DG__, __WEBPACK_EXTERNAL_MODULE_Y0hN__, __WEBPACK_EXTERNAL_MODULE_Y1_I__, __WEBPACK_EXTERNAL_MODULE_aYPB__, __WEBPACK_EXTERNAL_MODULE_cu1Y__, __WEBPACK_EXTERNAL_MODULE_ej3F__, __WEBPACK_EXTERNAL_MODULE_hl6F__, __WEBPACK_EXTERNAL_MODULE_iMF9__, __WEBPACK_EXTERNAL_MODULE_iURx__, __WEBPACK_EXTERNAL_MODULE_ioIi__, __WEBPACK_EXTERNAL_MODULE_jL6t__, __WEBPACK_EXTERNAL_MODULE_mdH0__, __WEBPACK_EXTERNAL_MODULE_qQaz__, __WEBPACK_EXTERNAL_MODULE_qhKH__, __WEBPACK_EXTERNAL_MODULE_qhWZ__, __WEBPACK_EXTERNAL_MODULE_u4yd__, __WEBPACK_EXTERNAL_MODULE_y39J__) {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -182,6 +182,7 @@ __webpack_require__("44oX");
 __webpack_require__("MZUI");
 __webpack_require__("Y1+I");
 __webpack_require__("Lgwm");
+__webpack_require__("tUNL");
 __webpack_require__("/DRg");
 __webpack_require__("Y0hN");
 __webpack_require__("JK3/");
@@ -392,6 +393,186 @@ module.exports = __WEBPACK_EXTERNAL_MODULE_qhKH__;
 /***/ (function(module, exports) {
 
 module.exports = __WEBPACK_EXTERNAL_MODULE_qhWZ__;
+
+/***/ }),
+
+/***/ "tUNL":
+/***/ (function(module, exports, __webpack_require__) {
+
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(true)
+		module.exports = factory(__webpack_require__("u4yd"));
+	else {}
+})(window, function(__WEBPACK_EXTERNAL_MODULE_u4yd__) {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "aAdN");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "aAdN":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var OpalForm_1;
+Object.defineProperty(exports, "__esModule", { value: true });
+const rionite_1 = __webpack_require__("u4yd");
+const template_rnt_1 = __webpack_require__("c9Lq");
+let OpalForm = OpalForm_1 = class OpalForm extends rionite_1.BaseComponent {
+    elementAttached() {
+        let focusTarget = this.$('focus') || this.$('input');
+        if (focusTarget) {
+            focusTarget.focus();
+            document.body.classList.remove('_noFocusHighlight');
+        }
+    }
+    validate() {
+        let focused = false;
+        return this.$$('input').reduce((valid, input) => {
+            let inputValid = input.validate();
+            if (!inputValid && !focused) {
+                input.focus();
+                focused = true;
+            }
+            return valid && inputValid;
+        }, true);
+    }
+};
+OpalForm.EVENT_SUBMIT = Symbol('submit');
+OpalForm = OpalForm_1 = __decorate([
+    rionite_1.Component({
+        template: template_rnt_1.default,
+        domEvents: {
+            form: {
+                submit(evt) {
+                    evt.preventDefault();
+                    if (this.validate()) {
+                        this.emit(OpalForm_1.EVENT_SUBMIT);
+                    }
+                }
+            }
+        }
+    })
+], OpalForm);
+exports.OpalForm = OpalForm;
+
+
+/***/ }),
+
+/***/ "c9Lq":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ("form:form {\nRnSlot:content\ninput (type=submit, style=display: none;)\n}");
+
+/***/ }),
+
+/***/ "u4yd":
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE_u4yd__;
+
+/***/ })
+
+/******/ });
+});
+
+/***/ }),
+
+/***/ "u4yd":
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE_u4yd__;
 
 /***/ }),
 
